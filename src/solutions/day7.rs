584e366ea50e9902805da1477323198a21ebd711
Hunter <HunterNL@users.noreply.github.com>
2023-10-18 17:13:11 +0200

    Upgrade NodeRef into a type

--- src/solutions/day7.rs
+++ src/solutions/day7.rs
@@ -13,23 +13,42 @@ struct Node {
     is_dir: bool,
 }
 
-type NodeRef = Rc<Node>;
+struct NodeRef(Rc<Node>);
 
-fn set_dir_sizes(node: &NodeRef) -> i32 {
-    if let Some(size) = node.size.get() {
-        return size;
+impl Clone for NodeRef {
+    fn clone(&self) -> Self {
+        Self(Rc::clone(&self.0))
+    }
+}
+
+impl NodeRef {
+    fn add_child(&self, path: impl Into<String>, size: Option<i32>, is_dir: bool) {
+        let self_cloned = Rc::clone(&self.0);
+        let child = Node::new(size, Some(Self(Rc::clone(&self_cloned))), is_dir);
+
+        self.0
+            .children
+            .borrow_mut()
+            .insert(path.into(), Self(Rc::new(child)));
     }
 
-    let size: i32 = node
-        .children
-        .borrow()
-        .iter()
-        .map(|f| set_dir_sizes(f.1))
-        .sum();
+    fn calc_size(&self) -> i32 {
+        if let Some(size) = self.0.size.get() {
+            return size;
+        }
 
-    node.size.set(Some(size));
+        let size: i32 = self
+            .0
+            .children
+            .borrow()
+            .iter()
+            .map(|f| f.1.calc_size())
+            .sum();
 
-    size
+        self.0.size.set(Some(size));
+
+        size
+    }
 }
 
 impl Node {
@@ -41,31 +60,6 @@ impl Node {
             is_dir,
         }
     }
-
-    fn calc_size(&self) -> i32 {
-        {
-            if let Some(size) = self.size.get() {
-                return size;
-            }
-        }
-
-        // let size = self.size.borrow_mut().unwrap();
-        let size: i32 = {
-            self.children
-                .borrow_mut()
-                .iter()
-                .map(|child| child.1.calc_size())
-                .sum()
-        };
-
-        self.size.set(Some(size));
-
-        size
-    }
-
-    fn add_child(&mut self, n: Rc<Self>, path: impl Into<String>) {
-        self.children.borrow_mut().insert(path.into(), n);
-    }
 }
 
 enum Command {
@@ -75,37 +69,13 @@ enum Command {
     Ls,
 }
 
-fn printnode(node: &NodeRef, indent: usize) {
-    let left_space = " ".repeat(indent * 4);
-    let size = node.size.get().unwrap_or(-1);
-
-    println!("{left_space}, {size}");
-
-    for child in &*node.children.borrow() {
-        printnode(child.1, indent + 1);
-    }
-}
-
 // https://adventofcode.com/2022/day/7
 pub fn solve(input: &str) -> Result<DayOutput, LogicError> {
     let fs = fun(input);
-    set_dir_sizes(&fs);
+    fs.calc_size();
 
     let countcell = Cell::new(0);
     sum_size(&fs, &countcell);
-
-    // let mut fs = HashMap::new();
-
-    // let root = VecNode {
-    //     size: None,
-    //     is_dir: true,
-    //     children: HashMap::new(),
-    //     parent: None,
-    // };
-
-    // fun(input);
-
-    // printnode(&fs, 0);
 
     let del_size = find_dir_to_delete(&fs);
 
@@ -116,11 +86,11 @@ pub fn solve(input: &str) -> Result<DayOutput, LogicError> {
 }
 
 fn sum_size(fs: &NodeRef, count: &Cell<i32>) {
-    if !fs.is_dir {
+    if !fs.0.is_dir {
         return;
     };
 
-    fs.size.get().and_then(|f: i32| {
+    fs.0.size.get().and_then(|f: i32| {
         if f <= 100_000 {
             count.set(count.get() + f);
         };
@@ -128,25 +98,25 @@ fn sum_size(fs: &NodeRef, count: &Cell<i32>) {
         None::<Option<i32>>
     });
 
-    for entry in &*fs.children.borrow() {
+    for entry in &*fs.0.children.borrow() {
         sum_size(entry.1, count);
     }
 }
 
 fn collect_fs_to_vec(fs: &NodeRef, v: &mut Vec<i32>) {
-    if !fs.is_dir {
+    if !fs.0.is_dir {
         return;
     }
 
-    v.push(fs.size.get().expect("size to be Some"));
+    v.push(fs.0.size.get().expect("size to be Some"));
 
-    for pair in &*fs.children.borrow() {
+    for pair in &*fs.0.children.borrow() {
         collect_fs_to_vec(pair.1, v);
     }
 }
 
 fn find_dir_to_delete(fs: &NodeRef) -> i32 {
-    let occupied_space = fs.size.get().expect("node to have size");
+    let occupied_space = fs.0.size.get().expect("node to have size");
     let storage_size = 70_000_000;
     let current_free_space = storage_size - occupied_space;
     let min_space_to_free = 30_000_000 - current_free_space;
@@ -164,52 +134,46 @@ fn find_dir_to_delete(fs: &NodeRef) -> i32 {
 }
 
 fn fun(input: &str) -> NodeRef {
-    let root: NodeRef = Rc::new(Node::new(None, None, true));
+    let root: NodeRef = NodeRef(Rc::new(Node::new(None, None, true)));
 
-    let mut current_node = Rc::clone(&root);
+    let mut current_node = NodeRef(Rc::clone(&root.0));
 
     input.lines().for_each(|f| {
         if f.as_bytes()[0] == b'$' {
             // println!("is cmd");
             match f {
-                "$ cd /" => current_node = Rc::clone(&root),
+                "$ cd /" => current_node = NodeRef(Rc::clone(&root.0)),
                 "$ ls" => (),
                 "$ cd .." => {
-                    current_node =
-                        Rc::clone(&current_node.parent.clone().expect("node to have a parent"));
+                    let parent_node = current_node
+                        .0
+                        .parent
+                        .clone()
+                        .expect("node to have a parent");
+                    current_node = parent_node;
                 }
                 _ => {
                     // cd $dirname
                     let (_, dirname) = f.split_at(5);
-                    let c2: NodeRef;
-                    {
-                        let child = current_node.children.borrow();
-                        c2 = Rc::clone(child.get(dirname).expect("dir to contain child"));
-                    }
 
-                    current_node = c2;
+                    current_node = current_node
+                        .clone()
+                        .0
+                        .children
+                        .borrow()
+                        .get(dirname)
+                        .expect("dir to have child")
+                        .clone();
                 }
             }
         } else {
             let (left, right) = f.split_once(' ').expect("line to split into two");
 
             if left == "dir" {
-                // Directory
-                let newnode = Node::new(None, Some(Rc::clone(&current_node)), true);
-
-                current_node
-                    .children
-                    .borrow_mut()
-                    .insert(right.to_owned(), Rc::new(newnode));
+                current_node.add_child(right, None, true);
             } else {
-                //File
                 let size: i32 = left.parse().expect("left side to parse into int");
-                let newnode = Node::new(Some(size), None, false);
-
-                current_node
-                    .children
-                    .borrow_mut()
-                    .insert(right.to_owned(), Rc::new(newnode));
+                current_node.add_child(right, Some(size), false);
             }
         }
     });
@@ -256,11 +220,9 @@ mod tests {
         .join("\n");
 
         let fs = fun(input.as_ref());
-        set_dir_sizes(&fs);
+        fs.calc_size();
 
-        assert_eq!(fs.size.get().unwrap_or_default(), 48381165);
-
-        set_dir_sizes(&fs);
+        assert_eq!(fs.0.size.get().unwrap_or_default(), 48381165);
 
         let countcell = Cell::new(0);
         sum_size(&fs, &countcell);
