75d0c0d49efa2760ae0df085a08da01744651890
Hunter <HunterNL@users.noreply.github.com>
2023-10-20 13:28:04 +0200

    Refactor away reference counters

--- src/solutions/day7.rs
+++ src/solutions/day7.rs
@@ -1,7 +1,6 @@
 use std::{
     cell::{Cell, OnceCell, RefCell},
     collections::HashMap,
-    rc::Rc,
     str::FromStr,
 };
 
@@ -13,101 +12,108 @@ enum Node {
     },
     Folder {
         size: OnceCell<i32>,
-        children: RefCell<HashMap<String, NodeRef>>,
+        children: RefCell<HashMap<String, Node>>,
     },
 }
+// Pops a directory from the end of the vector and move it into the new last entry in the vector
+fn pop_and_restore_dir(dirs: &mut Vec<(String, Node)>) {
+    let entry = dirs.pop().expect("dirs to have an entry");
+    dirs.last()
+        .expect("dirs to have a last")
+        .1
+        .add_child(entry.0, entry.1);
+}
 
-struct NodeRef(Rc<Node>);
-
-impl FromStr for NodeRef {
+impl FromStr for Node {
     type Err = String;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let node = Node::Folder {
+        let root = Self::Folder {
             size: OnceCell::new(),
             children: RefCell::new(HashMap::new()),
         };
 
-        let mut dirs = vec![Self::new(node)];
+        // Vector of all opened folders
+        // "Open" folders are removed from their original parent and put here while accessed
+        // It is important to "close" and return folders back to their original parent when popped
+        // Use `pop_and_restore_dir` for this
+        let mut dirs: Vec<(String, Self)> = vec![(String::new(), root)];
 
         s.lines().map(str::parse::<Line>).for_each(|entry| {
             let cmd = entry.expect("Succesfull parse");
             match cmd {
                 Line::Command(cmd) => match cmd {
                     Command::ChRoot => {
-                        dirs.drain(1..);
+                        while dirs.len() > 1 {
+                            pop_and_restore_dir(&mut dirs);
+                        }
                     }
-                    Command::ChUp => {
-                        dirs.pop();
-                    }
+                    Command::ChUp => pop_and_restore_dir(&mut dirs),
                     Command::ChDir(dir_name) => {
                         let child = dirs
                             .last()
                             .expect("Dirs to contain an item")
-                            .get_child(dir_name);
+                            .1
+                            .remove_child(dir_name);
                         dirs.push(child);
                     }
                     Command::Ls => (),
                 },
                 Line::DirEntry(dir_entry) => match dir_entry {
-                    DirEntry::File(name, size) => {
-                        dirs.last().expect("Dirs to contain an item").add_child(
-                            name,
-                            Some(size),
-                            false,
-                        );
-                    }
+                    DirEntry::File(name, size) => dirs
+                        .last()
+                        .expect("Dirs to contain an item")
+                        .1
+                        .add_child(name, Self::new(&NodeKind::File, size)),
                     DirEntry::Dir(name) => {
                         dirs.last()
                             .expect("Dirs to contain an item")
-                            .add_child(name, None, true);
+                            .1
+                            .add_child(name, Self::new(&NodeKind::Folder, 0));
                     }
                 },
             }
         });
 
-        Ok(dirs.remove(0))
+        // Important to ensure all opened dirs are back into their proper place
+        while dirs.len() > 1 {
+            pop_and_restore_dir(&mut dirs);
+        }
+
+        Ok(dirs.remove(0).1)
     }
 }
 
-impl NodeRef {
-    fn new(n: Node) -> Self {
-        Self(Rc::new(n))
-    }
+enum NodeKind {
+    File,
+    Folder,
 }
 
-impl Clone for NodeRef {
-    fn clone(&self) -> Self {
-        Self(Rc::clone(&self.0))
-    }
-}
-
-impl NodeRef {
-    fn add_child(&self, path: impl Into<String>, size: Option<i32>, is_dir: bool) {
-        let child: Node = if is_dir {
-            Node::Folder {
-                size: OnceCell::new(),
+impl Node {
+    fn new(kind: &NodeKind, size: i32) -> Self {
+        match kind {
+            NodeKind::File => Self::File { size },
+            NodeKind::Folder => Self::Folder {
+                size: OnceCell::new(), // Note ignoring the argument, unlike files, folder size is not known at creation. calc_size can figure that out
                 children: RefCell::new(HashMap::new()),
-            }
-        } else {
-            Node::File {
-                size: size.expect("File must have size provided"),
-            }
-        };
+            },
+        }
+    }
 
-        match self.0.as_ref() {
-            Node::File { .. } => panic!("Cannot add child to a file"),
-            Node::Folder { children, .. } => {
-                children.borrow_mut().insert(path.into(), Self::new(child));
+    fn add_child(&self, path: impl Into<String>, n: Self) {
+        match self {
+            Self::File { .. } => panic!("Cannot add child to a file"),
+            Self::Folder { children, .. } => {
+                children.borrow_mut().insert(path.into(), n);
             }
         }
     }
 
     // Get own size or resursively get (and cache) children's size
     fn calc_size(&self) -> i32 {
-        match self.0.as_ref() {
-            Node::File { size, .. } => *size,
-            Node::Folder { size, children, .. } => *size.get_or_init(|| {
+        match self {
+            Self::File { size, .. } => *size,
+            Self::Folder { size, children, .. } => *size.get_or_init(|| {
                 children
                     .borrow()
                     .iter()
@@ -117,14 +123,13 @@ impl NodeRef {
         }
     }
 
-    fn get_child(&self, path: impl Into<String>) -> Self {
-        match self.0.as_ref() {
-            Node::File { .. } => panic!("File doesn't have children"),
-            Node::Folder { children, .. } => children
-                .borrow()
-                .get(&path.into())
-                .expect("map to contain given child")
-                .clone(),
+    fn remove_child(&self, path: impl Into<String>) -> (String, Self) {
+        match self {
+            Self::File { .. } => panic!("File doesn't have children"),
+            Self::Folder { children, .. } => children
+                .borrow_mut()
+                .remove_entry(&path.into())
+                .expect("map to contain given child"),
         }
     }
 }
@@ -192,7 +197,7 @@ impl FromStr for Line {
 
 // https://adventofcode.com/2022/day/7
 pub fn solve(input: &str) -> Result<DayOutput, LogicError> {
-    let fs: NodeRef = input.parse().expect("Succesfull parse");
+    let fs: Node = input.parse().expect("Succesfull parse");
     let total_size = fs.calc_size();
 
     let countcell = Cell::new(0);
@@ -206,8 +211,8 @@ pub fn solve(input: &str) -> Result<DayOutput, LogicError> {
     })
 }
 
-fn sum_size(fs: &NodeRef, count: &Cell<i32>) {
-    match fs.0.as_ref() {
+fn sum_size(fs: &Node, count: &Cell<i32>) {
+    match fs {
         Node::File { .. } => (),
         Node::Folder { size, children, .. } => {
             let size = *size.get().expect("Size should be known at this point, if not NodeRef::calc_size should have been called first");
@@ -223,8 +228,8 @@ fn sum_size(fs: &NodeRef, count: &Cell<i32>) {
     }
 }
 
-fn collect_fs_to_vec(fs: &NodeRef, v: &mut Vec<i32>) {
-    match fs.0.as_ref() {
+fn collect_fs_to_vec(fs: &Node, v: &mut Vec<i32>) {
+    match fs {
         Node::File { .. } => (),
         Node::Folder { size, children, .. } => {
             v.push(*size.get().expect("size to exist"));
@@ -236,7 +241,7 @@ fn collect_fs_to_vec(fs: &NodeRef, v: &mut Vec<i32>) {
     }
 }
 
-fn find_dir_to_delete(fs: &NodeRef, occupied_space: i32) -> i32 {
+fn find_dir_to_delete(fs: &Node, occupied_space: i32) -> i32 {
     let storage_size = 70_000_000;
     let current_free_space = storage_size - occupied_space;
     let min_space_to_free = 30_000_000 - current_free_space;
@@ -291,7 +296,7 @@ mod tests {
         ]
         .join("\n");
 
-        let fs: NodeRef = input.parse().expect("Succesfull parse");
+        let fs: Node = input.parse().expect("Succesfull parse");
         let size = fs.calc_size();
 
         assert_eq!(size, 48_381_165);
