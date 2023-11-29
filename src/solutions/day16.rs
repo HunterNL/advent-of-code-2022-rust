448c78782f2736701d2357f67e9a91409fa52622
Hunter <HunterNL@users.noreply.github.com>
2023-11-29 19:57:47 +0100

    Simplify cave traversal
    Travel is now instant, no more goals or intermediary stops in empty caves

--- src/solutions/day16.rs
+++ src/solutions/day16.rs
@@ -11,11 +11,10 @@ use crate::parsing::consume_when;
 
 use super::{DayOutput, LogicError, PartResult};
 
+static START_CAVE: CaveName = CaveName('A', 'A');
+
 #[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
 struct CaveName(char, char);
-
-#[derive(Eq, Hash, PartialEq, Debug, Clone, Copy)]
-struct CaveId(usize);
 
 impl Display for CaveName {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -26,16 +25,15 @@ impl Display for CaveName {
     }
 }
 
-static START_CAVE: CaveName = CaveName('A', 'A');
+#[derive(Eq, Hash, PartialEq, Debug, Clone, Copy)]
+struct CaveId(usize);
 
 struct CaveSystem {
     caves: Vec<Cave>,
     caves_with_working_valve: Vec<CaveId>,
 }
 
-// struct SearchPath(Vec<usize>);
-
-fn find_shortest(caves: &[Cave], origin: usize, target: usize) -> usize {
+fn find_shortest(caves: &[Cave], origin: usize, target: usize) -> Vec<usize> {
     assert_ne!(origin, target);
     let mut frontier = VecDeque::new();
     frontier.push_back(vec![origin]);
@@ -44,11 +42,11 @@ fn find_shortest(caves: &[Cave], origin: usize, target: usize) -> usize {
         let cave_id = *path.last().unwrap();
 
         if cave_id == target {
-            return *path.get(1).unwrap();
+            return path;
         }
 
         let cave = caves.get(cave_id).unwrap();
-        cave.tunnels_id.iter().for_each(|tunnel_id| {
+        cave.tunnels.iter().for_each(|tunnel_id| {
             let mut new_path = path.clone();
             new_path.push(tunnel_id.0);
             frontier.push_back(new_path);
@@ -56,42 +54,6 @@ fn find_shortest(caves: &[Cave], origin: usize, target: usize) -> usize {
     }
 
     panic!("Path not found")
-}
-
-impl CaveSystem {
-    /// Goes overy every cave and assigns the new
-    fn generate_paths(caves: &mut Vec<Cave>) {
-        for origin in 0..caves.len() {
-            let mut paths = vec![];
-
-            for target in 0..caves.len() {
-                if origin == target {
-                    paths.push(0xDEADBEEF);
-                    continue;
-                }
-                paths.push(Self::best_step_towards(caves, origin, target));
-            }
-
-            caves.get_mut(origin).unwrap().paths = paths;
-        }
-    }
-    /// Finds the best step to take from the `origin` cave to the `target` cave
-    fn best_step_towards(caves: &[Cave], origin: usize, target: usize) -> usize {
-        find_shortest(caves, origin, target)
-    }
-
-    fn possible_goals(&self, valves_opened: u64) -> Vec<CaveId> {
-        let mut out = vec![];
-        for cave in &self.caves_with_working_valve {
-            if 1 << cave.0 & valves_opened == 0 {
-                out.push(*cave);
-            }
-        }
-        // if (out.len() < 6) {
-        //     println!("{}", out.len());
-        // }
-        out
-    }
 }
 
 impl Display for CaveSystem {
@@ -107,28 +69,12 @@ impl Display for CaveSystem {
 
 impl CaveSystem {
     fn from_str(input: &str) -> Self {
-        let mut caves: Vec<Cave> = input.lines().map(|l| l.parse::<Cave>().unwrap()).collect();
-        let mut name_map: HashMap<CaveName, CaveId> = HashMap::new();
+        let protocaves: Vec<CavePrototype> = input
+            .lines()
+            .map(|l| l.parse::<CavePrototype>().unwrap())
+            .collect();
 
-        caves.iter().enumerate().for_each(|(index, cave)| {
-            name_map.insert(cave.name, CaveId(index));
-        });
-
-        // For every cave, update its tunnels_by_id to refer to the index of
-        caves.iter_mut().for_each(|cave| {
-            let tunnels_by_id = cave
-                .tunnels_name
-                .iter()
-                .map(|tunnel_name| *name_map.get(tunnel_name).expect("To find matching name"))
-                .collect();
-
-            cave.tunnels_id = tunnels_by_id
-        });
-
-        caves
-            .iter_mut()
-            .enumerate()
-            .for_each(|(id, cave)| cave.id = CaveId(id));
+        let caves = Self::connect_protocaves(protocaves.as_slice());
 
         let caves_with_working_valve: Vec<CaveId> = caves
             .iter()
@@ -143,12 +89,77 @@ impl CaveSystem {
             .iter()
             .for_each(|id| default_valve_state |= 1 << id.0);
 
-        Self::generate_paths(&mut caves);
+        // Self::generate_paths(&mut caves);
 
         Self {
             caves,
             caves_with_working_valve,
         }
+    }
+
+    fn connect_protocaves(protocaves: &[CavePrototype]) -> Vec<Cave> {
+        let mut name_to_id_map: HashMap<CaveName, CaveId> = HashMap::new();
+
+        let mut caves: Vec<Cave> = protocaves
+            .iter()
+            .enumerate()
+            .map(|(pos, cave)| Cave {
+                id: CaveId(pos),
+                name: cave.name,
+                flow_rate: cave.flow_rate,
+                paths: vec![],
+                tunnels: vec![],
+                tunnels_by_name: cave.tunnels.clone(),
+            })
+            .collect();
+
+        for cave in &caves {
+            name_to_id_map.insert(cave.name, cave.id);
+        }
+
+        caves.iter_mut().for_each(|cave| {
+            cave.tunnels.extend(
+                cave.tunnels_by_name
+                    .iter()
+                    .map(|name| *name_to_id_map.get(name).unwrap()),
+            )
+        });
+
+        for origin_id in 0..caves.len() {
+            for target_id in 0..caves.len() {
+                if origin_id == target_id {
+                    caves.get_mut(origin_id).unwrap().paths.push(255);
+                    continue;
+                }
+
+                let shortest_path = find_shortest(caves.as_slice(), origin_id, target_id);
+
+                caves
+                    .get_mut(origin_id)
+                    .unwrap()
+                    .paths
+                    .push(shortest_path.len() as u32)
+            }
+        }
+
+        caves
+    }
+
+    fn cave_by_name(&self, cave_name: CaveName) -> Option<CaveId> {
+        self.caves
+            .iter()
+            .position(|cave| cave.name == cave_name)
+            .map(CaveId)
+    }
+
+    fn possible_goals(&self, valves_opened: u64) -> Vec<CaveId> {
+        let mut out = vec![];
+        for cave in &self.caves_with_working_valve {
+            if 1 << cave.0 & valves_opened == 0 {
+                out.push(*cave);
+            }
+        }
+        out
     }
 }
 
@@ -157,9 +168,9 @@ struct Cave {
     id: CaveId,
     name: CaveName,
     flow_rate: u32,
-    tunnels_name: Vec<CaveName>,
-    tunnels_id: Vec<CaveId>,
-    paths: Vec<usize>,
+    paths: Vec<u32>,      // Length of paths to other caves
+    tunnels: Vec<CaveId>, // Direct neighbours
+    tunnels_by_name: Vec<CaveName>,
 }
 
 impl Display for Cave {
@@ -168,7 +179,7 @@ impl Display for Cave {
         f.write_fmt(format_args!("{}", self.flow_rate))?;
         f.write_str(" tunnels are ")?;
         let a = self
-            .tunnels_id
+            .tunnels
             .iter()
             .map(|t| t.0.to_string())
             .collect::<Vec<String>>()
@@ -186,7 +197,6 @@ struct Path {
     valves_opened_count: usize,
     relieved_pressure: u32,
     position: CaveId,
-    goal: Option<CaveId>,
 }
 
 fn possible_goals(valves_opened: u64, cave_count: u32) -> Vec<CaveId> {
@@ -202,46 +212,31 @@ fn possible_goals(valves_opened: u64, cave_count: u32) -> Vec<CaveId> {
 // //u32  PPPPPPPP
 
 impl Path {
-    #[must_use]
-    fn open_valve(&self, id: CaveId, rate: u32) -> Self {
+    fn open_valve(&mut self, id: CaveId, rate: u32) {
         let valve: u64 = 1 << id.0;
 
-        Self {
-            minutes: self.minutes + 1,
-            open_valve_rate: self.open_valve_rate + rate,
-            valves_opened: self.valves_opened | valve,
-            // valves_remaining: self.valves_remaining & ^valve,
-            valves_opened_count: self.valves_opened_count + 1,
-            relieved_pressure: self.relieved_pressure + self.open_valve_rate, // Exclude the rate, as the newly opened valve doesn't count yet
-            ..*self
-        }
+        // Self {
+        self.minutes += 1;
+        self.relieved_pressure += self.open_valve_rate;
+        self.open_valve_rate += rate;
+        self.valves_opened |= valve;
+        self.valves_opened_count += 1;
     }
 
-    fn travel(&self, destination: CaveId) -> Self {
-        Self {
-            minutes: self.minutes + 1,
+    fn travel(&self, duration: u32, destination: CaveId, rate: u32) -> Self {
+        let mut a = Self {
+            minutes: self.minutes + duration,
             position: destination,
-            relieved_pressure: self.relieved_pressure + self.open_valve_rate,
+            relieved_pressure: self.relieved_pressure + (self.open_valve_rate * duration),
             ..*self
-        }
+        };
+        a.open_valve(destination, rate);
+        a
     }
 
     fn idle(&self) -> u32 {
         let minutes_to_idle = MAX_CAVE_TIME - self.minutes;
         self.relieved_pressure + self.open_valve_rate * minutes_to_idle
-
-        // Self {
-        //     minutes: self.minutes + minutes_to_idle,
-        //     relieved_pressure: ,
-        //     ..*self
-        // }
-    }
-
-    fn set_goal(&self, goal_cave: Option<CaveId>) -> Self {
-        Self {
-            goal: goal_cave,
-            ..*self
-        }
     }
 
     fn has_opened_valve(&self, id: usize) -> bool {
@@ -249,42 +244,42 @@ impl Path {
     }
 
     fn is_done(&self) -> bool {
-        self.minutes == MAX_CAVE_TIME || self.goal.is_none()
+        self.minutes == MAX_CAVE_TIME // || self.goal.is_none()
     }
 
     fn append_futures(&self, cave_system: &CaveSystem, queue: &mut Vec<Path>) {
-        assert!(!self.is_done());
+        if self.is_done() {
+            return;
+        }
 
         let current_cave = cave_system.caves.get(self.position.0).unwrap();
 
-        // We've reached the goal
-        if self.goal.is_some_and(|goal| goal == self.position) {
-            let path = self.open_valve(self.position, current_cave.flow_rate);
-
-            let goals = cave_system.possible_goals(path.valves_opened);
-
-            // We're done
-            if goals.is_empty() {
-                queue.push(path.set_goal(None));
-            } else {
-                //Pick a new goal
-                for goal in goals {
-                    queue.push(path.set_goal(Some(goal)));
-                }
-            }
-        } else {
-            // Else travel to the goal
-            let next_id = current_cave
-                .paths
-                .get(self.goal.expect("Goal to exsist").0)
-                .expect("Path to have a path to goal");
-
-            queue.push(self.travel(CaveId(*next_id)));
-        }
+        queue.extend(
+            cave_system
+                .caves_with_working_valve
+                .iter()
+                .filter(|cave_id| !self.has_opened_valve(cave_id.0))
+                .filter_map(|target_id| {
+                    // let target_cave = cave_system.caves.get(target_id.0).unwrap();
+                    let travel_time = current_cave.paths.get(target_id.0).unwrap() - 1;
+                    let target = cave_system.caves.get(target_id.0).unwrap();
+                    // smallest_action_time = smallest_action_time.min(*path_len); // SIDE EFFECT
+                    if self.minutes + travel_time + 1 > MAX_CAVE_TIME {
+                        None
+                    } else {
+                        Some(self.travel(travel_time, *target_id, target.flow_rate))
+                    }
+                }),
+        );
     }
 }
+struct CavePrototype {
+    name: CaveName,
+    tunnels: Vec<CaveName>,
+    flow_rate: u32,
+}
 
-impl FromStr for Cave {
+impl FromStr for CavePrototype {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -308,45 +303,39 @@ impl FromStr for Cave {
             tunnels.push(CaveName(*id.first().unwrap(), *id.last().unwrap()))
         }
 
-        Ok(Cave {
-            id: CaveId(0xDEADBEEF), //I'm prematurely optimizing Option away without measuring or profiling, sue me
+        Ok(Self {
             flow_rate,
             name,
-            tunnels_name: tunnels,
-            tunnels_id: vec![],
-            paths: vec![],
+            tunnels,
         })
     }
 }
 
 fn find_biggest_release(cave_system: CaveSystem) -> u32 {
     let start_cave_id = cave_system
-        .caves
-        .iter()
-        .position(|cave| cave.name == START_CAVE)
+        .cave_by_name(START_CAVE)
         .expect("start cave should be present in cave_system");
 
-    let mut queue = vec![];
-
-    let goals = cave_system.possible_goals(0);
-
-    queue.extend(goals.into_iter().map(|caveid| Path {
+    let mut queue = vec![Path {
         minutes: 0,
         open_valve_rate: 0,
         valves_opened: 0,
         valves_opened_count: 0,
         relieved_pressure: 0,
-        position: CaveId(start_cave_id),
-        goal: Some(caveid),
-    }));
+        position: start_cave_id,
+    }];
 
     let mut biggest_release: u32 = 0;
 
     while let Some(path) = queue.pop() {
-        if path.is_done() {
+        let queue_size = queue.len();
+        path.append_futures(&cave_system, &mut queue);
+        let new_queue_size = queue.len();
+
+        let is_done = queue_size == new_queue_size;
+
+        if is_done {
             biggest_release = biggest_release.max(path.idle())
-        } else {
-            path.append_futures(&cave_system, &mut queue);
         }
     }
 
@@ -356,7 +345,10 @@ fn find_biggest_release(cave_system: CaveSystem) -> u32 {
 // https://adventofcode.com/2022/day/16
 pub fn solve(input: &str) -> Result<DayOutput, LogicError> {
     let caves = CaveSystem::from_str(input);
-    // caves.generate_paths();
+    println!(
+        "Caves with valves: {}",
+        caves.caves_with_working_valve.len()
+    );
     let pressure = find_biggest_release(caves);
 
     Ok(DayOutput {
@@ -396,4 +388,32 @@ Valve JJ has flow rate=21; tunnel leads to valve II";
 
         assert_eq!(pressure, 1651);
     }
+
+    // fn get_cave_id()
+
+    // #[test]
+    // fn sequence() {
+    //     let caves = CaveSystem::from_str(EXAMPLE_INPUT);
+    //     let start_cave_id = caves.cave_by_name(super::CaveName('A', 'A')).unwrap();
+    //     let first_cave_id = caves.cave_by_name(super::CaveName('D', 'D')).unwrap();
+
+    //     let mut path = Path {
+    //         minutes: 0,
+    //         open_valve_rate: 0,
+    //         valves_opened: 0,
+    //         valves_opened_count: 0,
+    //         relieved_pressure: 0,
+    //         position: start_cave_id,
+    //     };
+
+    //     assert_eq!(path.idle(), 0);
+
+    //     path = path.travel(1, start_cave_id, 20);
+    //     assert_eq!(path.minutes, 2);
+    // assert_eq!(path.relieved_pressure, 0)
+
+    // path = path.travel(2, destination, rate)
+
+    // path.
+    // }
 }
